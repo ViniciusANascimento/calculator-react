1419acc50e9dd09c63be3505d4b45199a60bbf73
Vinicius Nascimento <vini.a.nasci@gmail.com>
2025-03-19 12:16:40 -0300

    feat: add Input component

---
+++ src/components/Input/style.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+export const InputContainer = styled.div`
+  width: 100%;
+  height: 85px;
+  background-color: #AAAAFF;
+
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+
+  font-size: 24px;
+  font-family: 'Roboto', sans-serif;
+
+  input {
+    width: 100%;
+    height: 85px;
+    background-color: #AAAAFF;
+    border: 0;
+    padding: 0 10px;
+
+    font-size: 24px;
+    font-family: 'Roboto', sans-serif;
+    color: #FFFFFF;
+  }
+`
